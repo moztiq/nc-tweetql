73949343377a8fd298df5302fa432b4619435b45
Seongki Kim <moztiq@gmail.com>
2023-11-09 13:42:28 +0900

    query resolver

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 import { ApolloServer, gql } from 'apollo-server';
 
+const tweets = [
+  {
+    id: '1',
+    text: 'hello',
+    // author:
+  },
+  {
+    id: '2',
+    text: 'hello 2',
+    // author:
+  },
+];
+
 const typeDefs = gql`
   type User {
     id: ID!
@@ -10,18 +23,31 @@ const typeDefs = gql`
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
   type Query {
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
+    ping: String!
   }
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
     deleteTweet(id: ID!): Boolean!
   }
 `;
-const server = new ApolloServer({ typeDefs });
+
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, args) {
+      return tweets.find((tweet) => tweet.id === args.id);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
 });
